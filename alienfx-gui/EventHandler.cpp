d619f2c600a74729847903582d55cff53dbe1a3d
Linooos <lin67399592@icloud.com>
2025-07-02 01:11:58 +0800

    fix the profile switch issue.

--- alienfx-gui/EventHandler.cpp
+++ alienfx-gui/EventHandler.cpp
@@ -108,6 +108,7 @@ void EventHandler::SwitchActiveProfile(profile* newID, bool force)
 void EventHandler::ToggleFans() {
 	if (mon) {
 		//mon->SetCurrentMode();
+		mon->SetPowerMode(fan_conf->lastProf->powerSet);
 		if (conf->fansOnBattery || conf->statePower)
 			mon->Start();
 		else
